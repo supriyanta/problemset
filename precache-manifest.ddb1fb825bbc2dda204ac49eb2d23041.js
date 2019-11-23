03b596688490746cfdb800cb9c17d1dfc5bc5032
supriyanta <supriyanta34@gmail.com>
2019-11-23 13:28:35 +0530

    Updates

---
+++ precache-manifest.ddb1fb825bbc2dda204ac49eb2d23041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af901d111191cdd78e917c7a585f46f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e63ea300c53f79ae64c",
+    "url": "/static/css/main.f6722760.chunk.css"
+  },
+  {
+    "revision": "ceed40a7f1bfe3905ab0",
+    "url": "/static/js/2.12bbfa6e.chunk.js"
+  },
+  {
+    "revision": "1e63ea300c53f79ae64c",
+    "url": "/static/js/main.be742831.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
